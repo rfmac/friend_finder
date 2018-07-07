0b1b8a21a15b679dbf2bbe70e0ce3b51460e9dcd
rfmac <rfennmacdougall@gmail.com>
2018-07-07 08:59:01 -0500

    commit

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+// TV Sitcoms "Friends" and "The Big Bang Theory"
+
+var friends = [
+	{
+	name: "Rachel",
+	photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	scores: [
+	"5",
+	"1",
+	"4",
+	"4",
+	"5",
+	"1",
+	"2",
+	"5",
+	"4",
+	"1"
+	]
+	},
+	{
+	name: "Monica",
+	photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+	scores: [
+	"4",
+	"2",
+	"5",
+	"1",
+	"3",
+	"2",
+	"2",
+	"1",
+	"3",
+	"2"
+	]
+	},
+	{
+	name: "Phoebe",
+	photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+	scores: [
+	"5",
+	"2",
+	"2",
+	"2",
+	"4",
+	"1",
+	"3",
+	"2",
+	"5",
+	"5"
+	]
+	},
+	{
+	name: "Joey",
+	photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+	scores: [
+	"3",
+	"3",
+	"4",
+	"2",
+	"2",
+	"1",
+	"3",
+	"2",
+	"2",
+	"3"
+	]
+	},
+	{
+	name: "Chandler",
+	photo: "https://m.facebook.com/photo.php?fbid=10208500699025296&id=1542229019&set=a.1549418665704.77596.1542229019&source=11",
+	scores: [
+	"4",
+	"3",
+	"4",
+	"1",
+	"5",
+	"2",
+	"5",
+	"3",
+	"1",
+	"4"
+	]
+	},
+	{
+	name: "Ross",
+	photo: "https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/12741971_10205764267089153_4212986785721953092_n.jpg?oh=4e18265f7d380167223a97fbd7eba278&oe=57B78445",
+	scores: [
+	"4",
+	"4",
+	"2",
+	"3",
+	"2",
+	"2",
+	"3",
+	"2",
+	"4",
+	"5"
+	]
+	},
+	{
+	name: "Sheldon",
+	photo: "http://i.ebayimg.com/images/g/J1wAAOSwpDdVbZvG/s-l300.jpg",
+	scores: [
+	"5",
+	"2",
+	"4",
+	"1",
+	"4",
+	"1",
+	"2",
+	"1",
+	"5",
+	"5"
+	]
+	},
+	{
+	name: "Penny",
+	photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+	scores: [
+	"4",
+	"2",
+	"5",
+	"1",
+	"3",
+	"2",
+	"5",
+	"1",
+	"3",
+	"2"
+	]
+	},
+	{
+	name: "Leonard",
+	photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+	scores: [
+	"5",
+	"2",
+	"2",
+	"1",
+	"4",
+	"1",
+	"3",
+	"2",
+	"5",
+	"5"
+	]
+	},
+	{
+	name: "Raj",
+	photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+	scores: [
+	"1",
+	"3",
+	"4",
+	"2",
+	"2",
+	"1",
+	"3",
+	"2",
+	"2",
+	"3"
+	]
+	}
+]
